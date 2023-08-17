8cb9a24ae3cab019439bc4bdf7ccaa91b677c04b
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-17 17:30:57 +0200

    Add routes & connectDB function

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
-
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
+const connectDB = require('./db/connect')
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -15,13 +17,17 @@ app.get('/', (req, res) => {
   res.send('jobs api');
 });
 
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', jobsRouter)
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-  try {
+  try { 
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
