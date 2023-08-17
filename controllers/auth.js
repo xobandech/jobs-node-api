0fd6f0a09f177ba651706ff940695d36d1314abe
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-17 21:22:20 +0200

    Add register logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,14 @@
-const register = (req, res) => {
-  res.send("register");
+const { StatusCodes } = require("http-status-codes");
+const User = require("../models/User");
+const { BadRequestError } = require("../errors")
+
+const register = async (req, res) => {
+    const { name, email, password } = req.body
+    if (!name || !email || !password) {
+      throw new BadRequestError("Please provide name, email and password")
+    }
+    const user = await User.create({ ...req.body })
+    res.status(StatusCodes.CREATED).json({user})
 };
 
 const login = (req, res) => {
