cfcd37bb3fe7094a850dd66af216745b5a068945
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-17 21:48:50 +0200

    As i add pre register middleware, delete unnecessary code

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,12 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
-const { BadRequestError } = require("../errors")
+const { BadRequestError } = require("../errors");
+const { genSalt, hash } = require("bcryptjs");
 
 const register = async (req, res) => {
-    const { name, email, password } = req.body
-    if (!name || !email || !password) {
-      throw new BadRequestError("Please provide name, email and password")
-    }
-    const user = await User.create({ ...req.body })
-    res.status(StatusCodes.CREATED).json({user})
+  const user = await User.create({ ...req.body });
+
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = (req, res) => {
