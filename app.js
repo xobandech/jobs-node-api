e2cf76cb588eae01b29cad79187ccca321a6dfa3
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-21 13:28:49 +0200

    Prettier + add authMiddleware to jobs route

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
-require('dotenv').config();
-require('express-async-errors');
-const express = require('express');
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
 const app = express();
-const authRouter = require('./routes/auth')
-const jobsRouter = require('./routes/jobs')
-const connectDB = require('./db/connect')
-const morgan = require('morgan')
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/jobs");
+const connectDB = require("./db/connect");
+const morgan = require("morgan");
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const authMiddleware = require("./middleware/authentication");
 
 app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
+app.get("/", (req, res) => {
+  res.send("jobs api");
 });
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/jobs', jobsRouter)
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", authMiddleware, jobsRouter);
 
+app.use(authMiddleware);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-  try { 
-    await connectDB(process.env.MONGO_URI)
+  try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
