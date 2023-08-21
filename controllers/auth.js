986edc92fad402a7542c1db35f5ebeb84d064558
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-21 12:36:30 +0200

    Create login method

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,29 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
-const { BadRequestError } = require("../errors");
-const { genSalt, hash } = require("bcryptjs");
+const { BadRequestError, UnauthenticatedError } = require("../errors/")
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-
-  res.status(StatusCodes.CREATED).json({ user });
+  res.status(StatusCodes.CREATED).json({ user: { name: user.getName(), email: user.email } , token: user.createJWT() });
 };
 
-const login = (req, res) => {
-  res.send("login");
-};
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+  if(!email || !password) {
+    throw new BadRequestError("Please provide email and password.")
+  }
+
+  const user = await User.findOne({email})
+  if (!user) {
+    throw new UnauthenticatedError("User is  not registered or Invalid Credentials.")
+  }
+  const token = user.createJWT()
+  res.status(StatusCodes.OK).json({ user: { name: user.getName()}, token})
+}
+catch(e) {
+  console.log(e);
+}};
 
 module.exports = {
   register,
