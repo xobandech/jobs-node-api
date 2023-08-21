4dd722b5fa1ab6ee1a22d08f95130d89b2b888d8
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-21 13:39:01 +0200

    Create auth middleware

---
+++ middleware/authentication.js
@@ -0,0 +1,24 @@
+const { UnauthenticatedError } = require("../errors")
+const User = require("../models/User")
+const jwt = require("jsonwebtoken")
+
+
+const authMiddleware = async (req, res, next) => {
+    const authHeader = req.headers.authorization
+    console.log(authHeader);
+    if (!authHeader.startsWith('Bearer ') || !authHeader) {
+        throw new UnauthenticatedError("Invalid auth headers")
+    }
+    const token = authHeader.split(' ')[1]
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = {userId: payload.userId, name: payload.name}
+        console.log(payload);
+        console.log("dasd");
+    } catch (e) {
+        throw new UnauthenticatedError("Invalid auth headers")
+    }
+    next()
+}
+
+module.exports = authMiddleware
