3e09817087cd88f4342714b8ae15a467096b9819
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-25 16:35:57 +0200

    Add handleRegister

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,32 +1,51 @@
-import { useState, ChangeEvent } from "react";
+import { useState, ChangeEvent, FormEvent } from "react";
 import FormInput from "../components/FormInput";
 const defaultFormFields = {
-  username: "",
+  name: "",
   email: "",
   password: "",
   confirmPassword: "",
 };
 const RegisterPage = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
-  const { username, email, password, confirmPassword } = formFields;
-
+  const { name, email, password, confirmPassword } = formFields;
+  const [responseOk, setResponseOk] = useState<null | boolean>(null)
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setFormFields({ ...formFields, [name]: value });
     console.log(formFields);
   };
 
+  const handleRegister = async (e: FormEvent) => {
+    e.preventDefault()
+    if (password == confirmPassword) {
+      await fetch("http://localhost:3001/api/v1/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            name, email, password
+        })
+      }).then(res => res.ok ? setResponseOk(true) : setResponseOk(false));
+    }
+    console.log({
+        name, email, password
+    });
+    
+  };
+
   return (
     <div>
-      <form className="flex max-w-[300px] flex-col [&>.input]:mb-3">
+      <form id="myForm" onSubmit={(e) => handleRegister(e)} className="flex max-w-[300px] flex-col [&>.input]:mb-3">
         <FormInput
           onChange={handleChange}
-          label="Username"
+          label="Name"
           type="text"
-          name="username"
-          value={username}
+          name="name"
+          value={name}
           required
-          shrinkLabel={Boolean(username)}
+          shrinkLabel={Boolean(name)}
         />
         <FormInput
           onChange={handleChange}
@@ -55,10 +74,11 @@ const RegisterPage = () => {
           required
           shrinkLabel={Boolean(confirmPassword)}
         />
-        <button className="w-20 mx-auto h-10 rounded-md bg-violet-200">
+        <button type="submit" className="w-20 mx-auto h-10 rounded-md bg-violet-200">
           Register
         </button>
       </form>
+      { responseOk == true ? "Successfull register" : responseOk == false ? "Register failed..." : ""}
     </div>
   );
 };
