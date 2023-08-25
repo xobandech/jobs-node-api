c881a76de2b282a6e6d8e8085a9efacf7c46b429
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-25 20:21:04 +0200

    Wrap App in UserContext

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,18 @@ import JobsPage from "./pages/JobsPage";
 import LoginPage from "./pages/LoginPage";
 import NavBar from "./components/NavBar";
 import RegisterPage from "./pages/RegisterPage";
+import UserProvider from "./contexts/UserContext";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<NavBar />}>
-        <Route path="/jobs" element={<JobsPage />} />
-        <Route path="/auth/login" element={<LoginPage />}/>
-        <Route path="/auth/register" element={<RegisterPage />}/>
-      </Route>
-    </Routes>
+    <UserProvider>
+      <Routes>
+        <Route path="/" element={<NavBar />}>
+          <Route path="/jobs" element={<JobsPage />} />
+          <Route path="/auth/login" element={<LoginPage />} />
+          <Route path="/auth/register" element={<RegisterPage />} />
+        </Route>
+      </Routes>
+    </UserProvider>
   );
 }
 
