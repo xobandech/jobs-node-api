4360410af0b2e15c7b7f3644a1fc8d5556bef4da
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-25 12:37:56 +0200

    Add Pages to Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router-dom";
+import JobsPage from "./pages/JobsPage";
+import LoginPage from "./pages/LoginPage";
+import NavBar from "./components/NavBar";
+import RegisterPage from "./pages/RegisterPage";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={NavBar}>
-        <Route path="/jobs" element={JobsPage} />
-        <Route path="/auth/login" element={LoginPage}/>
-        <Route path="/auth/register" element={RegisterPage}/>
+      <Route path="/" element={<NavBar />}>
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/auth/login" element={<LoginPage />}/>
+        <Route path="/auth/register" element={<RegisterPage />}/>
       </Route>
     </Routes>
   );
