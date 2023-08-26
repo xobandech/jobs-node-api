fd10549113312452fd7f74e41e2a6db4fe209728
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-26 13:49:10 +0200

    Moved localStorage user detection logic from RegisterForm to RegisterPage

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,10 +1,23 @@
 import RegisterForm from "../components/RegisterForm";
-
+import jwtDecode from "jwt-decode";
+import { useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
+import { User } from "../contexts/UserContext";
 const RegisterPage = () => {
-
+  const { setCurrentUser } = useContext(UserContext);
+  const localToken = localStorage.getItem("JTWToken");
+  if (localToken) {
+    const decodeToken = async () => {
+      const user = (await jwtDecode(localToken)) as User;
+      setCurrentUser(user);
+      window.location.replace("/jobs");
+    };
+    decodeToken();
+  }
+  if (localToken) return <div></div>
   return (
     <div className="flex items-center justify-center">
-        <RegisterForm />
+      <RegisterForm />
     </div>
   );
 };
