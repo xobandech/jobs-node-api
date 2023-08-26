871c7b7c3a0a84b838940bf8763cebbf05197fe7
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-26 15:03:14 +0200

    Add 404 page route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import JobsPage from "./pages/JobsPage";
 import LoginPage from "./pages/LoginPage";
 import NavBar from "./components/NavBar";
 import RegisterPage from "./pages/RegisterPage";
 import UserProvider from "./contexts/UserContext";
+import NotFoundPage from "./pages/NotFoundPage";
 function App() {
   return (
     <UserProvider>
@@ -12,6 +13,7 @@ function App() {
           <Route path="/jobs" element={<JobsPage />} />
           <Route path="/auth/login" element={<LoginPage />} />
           <Route path="/auth/register" element={<RegisterPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </UserProvider>
