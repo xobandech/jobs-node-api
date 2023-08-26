5bff8282b30a6369d97124aa8fa23b05eb03c092
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-26 17:31:03 +0200

    small fixes

--- node-api-server/controllers/jobs.js
+++ node-api-server/controllers/jobs.js
@@ -3,50 +3,60 @@ const Job = require("../models/Job");
 const { NotFoundError, BadRequestError } = require("../errors");
 
 const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({createdBy: req.user.userId}).sort('createdAt')
-  res.status(StatusCodes.OK).json({jobs, count: jobs.length})
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 
 const createJob = async (req, res) => {
-
-  req.body.createdBy = req.user.userId
+  req.body.createdBy = req.user.userId;
   console.log(req.body);
-  const job = await Job.create(req.body)
-  res.status(StatusCodes.CREATED).json(job)
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json(job);
 };
 
 const getJob = async (req, res) => {
-  const user = req.user
-  const jobId = req.params.id
-  const job = await Job.find({_id: jobId, createdBy: user.userId})
-  if(!job) {
-    throw new NotFoundError(`No job with id ${jobId}`)
+  const user = req.user;
+  const jobId = req.params.id;
+  const job = await Job.find({ _id: jobId, createdBy: user.userId });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
   }
-  res.status(StatusCodes.OK).json(job)
+  res.status(StatusCodes.OK).json(job);
 };
 
 const updateJob = async (req, res) => {
-  const user = req.user
-  const jobId = req.params.id
-  const job = await Job.findOne({_id: jobId, createdBy: user.userId})
-  if(!job) {
-    throw new NotFoundError(`You dont have access to job with id ${jobId}`)
+  try {
+    const user = req.user;
+    const jobId = req.params.id;
+    const job = await Job.findOne({ _id: jobId, createdBy: user.userId });
+    if (!job) {
+      throw new NotFoundError(`You dont have access to job with id ${jobId}`);
+    }
+    const data = req.body;
+    console.log(data);
+    if (!data.company && !data.position && !data.status)
+      throw new BadRequestError(
+        "Please provide company and/or position and/or status"
+      );
+    const modifiedJob = await Job.findByIdAndUpdate(jobId, data, { new: true });
+    res.status(StatusCodes.OK).json(modifiedJob);
+  } catch (e) {
+    console.log(e);
   }
-  const data = req.body
-  if (!data.company && !data.position) throw new BadRequestError("Please provide company and/or position")
-  const modifiedJob = await Job.findByIdAndUpdate(jobId, data, { new: true });
-  res.status(StatusCodes.OK).json(modifiedJob)
 };
 
 const deleteJob = async (req, res) => {
-  const user = req.user
-  const jobId = req.params.id
-  const deletedJob = await Job.findByIdAndDelete({_id: jobId, createdBy: user.userId})
+  const user = req.user;
+  const jobId = req.params.id;
+  const deletedJob = await Job.findByIdAndDelete({
+    _id: jobId,
+    createdBy: user.userId,
+  });
   console.log(deletedJob);
-  if(!deletedJob) {
-    throw new NotFoundError(`You dont have access to job with id ${jobId}`)
+  if (!deletedJob) {
+    throw new NotFoundError(`You dont have access to job with id ${jobId}`);
   }
-  res.status(StatusCodes.NO_CONTENT).json({msg: "Job deleted successfully"})
+  res.status(StatusCodes.NO_CONTENT).json({ msg: "Job deleted successfully" });
 };
 
 module.exports = {
@@ -54,5 +64,5 @@ module.exports = {
   getAllJobs,
   getJob,
   updateJob,
-  deleteJob
+  deleteJob,
 };
