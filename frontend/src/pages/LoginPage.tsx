a1e284d97af5cc5027402521baea91c21065c573
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-26 15:13:12 +0200

    Add detecting user in localStorage logic

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,7 +1,24 @@
-import LoginForm from "../components/LoginForm"
+import jwtDecode from "jwt-decode";
+import { useContext } from "react";
+import LoginForm from "../components/LoginForm";
+import { UserContext, User } from "../contexts/UserContext";
+
 const LoginPage = () => {
-    return <div className="flex justify-center">
-        <LoginForm />
+  const { setCurrentUser } = useContext(UserContext);
+  const localToken = localStorage.getItem("JTWToken");
+  if (localToken) {
+    const decodeToken = async () => {
+      const user = (await jwtDecode(localToken)) as User;
+      setCurrentUser(user);
+      window.location.replace("/jobs");
+    };
+    decodeToken();
+  }
+  if (localToken) return <div></div>;
+  return (
+    <div className="flex justify-center">
+      <LoginForm />
     </div>
-}
-export default LoginPage
+  );
+};
+export default LoginPage;
