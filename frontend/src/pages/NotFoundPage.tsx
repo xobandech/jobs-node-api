54e43f4c84995c360ecdc0a88a97f25020fac791
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-26 15:11:43 +0200

    Create 404 page

---
+++ frontend/src/pages/NotFoundPage.tsx
@@ -0,0 +1,7 @@
+const NotFoundPage = () => {
+    return <div className="flex text-3xl font-mono flex-col justify-center items-center text-center min-h-[95vh]">
+        <h1>404 PAGE NOT FOUND</h1>
+    </div>
+}
+
+export default NotFoundPage
