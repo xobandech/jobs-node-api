3306c7e5a8270dfb488b0e98d354d90d1f59b234
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-26 15:58:31 +0200

    Move existed types and create Job type

---
+++ frontend/src/types/types.tsx
@@ -0,0 +1,24 @@
+export type User = {
+  name: string;
+  userId: string;
+};
+
+export type UserData = {
+  user: User;
+  token: string;
+};
+
+export type UserContextType = {
+  token: string;
+  setToken: (token: string) => void;
+  currentUser: User;
+  setCurrentUser: (arg0: User) => void;
+};
+
+export type Job = {
+    status: string
+    company: string
+    position: string
+    createdAt: Date
+    updatedAt: Date
+}
