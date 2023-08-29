e6948cd7d73f979ba1d043cd3e750bdbeb8b6392
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-29 11:00:41 +0200

    Fix update logic & add some styling

--- frontend/src/pages/JobsPage.tsx
+++ frontend/src/pages/JobsPage.tsx
@@ -32,26 +32,25 @@ const JobsPage = () => {
           }, 1500);
       });
       socket.on("sendUpdatedJob", function (updatedJob: Job) {
-        setJobsData(prevJobsData => {
+        setJobsData((prevJobsData) => {
           if (!prevJobsData?.jobs) return prevJobsData;
-      
+
           const existingJobs = prevJobsData.jobs;
           const updatedJobs = [...existingJobs];
-          
-          const updatedJobIndex = updatedJobs.findIndex(job => job._id === updatedJob._id);
+
+          const updatedJobIndex = updatedJobs.findIndex(
+            (job) => job._id === updatedJob._id
+          );
           if (updatedJobIndex !== -1) {
             updatedJobs[updatedJobIndex] = updatedJob;
           }
-      
+
           return {
             jobs: updatedJobs,
-            count: (prevJobsData.count || 0) + 1
+            count: (prevJobsData.count || 0) + 1,
           };
         });
       });
-      
-      
-      
     });
   }, []);
 
@@ -70,11 +69,10 @@ const JobsPage = () => {
   }, []);
 
   return (
-    <div className="">
-      <NewJobForm />
-      <div className="w-full">
-        <div className="flex justify-center text-xl font-semibold">Jobs</div>
-        <div className="grid grid-cols-4 max-xl:grid-cols-3 max-sm:flex max-sm:items-center max-sm:flex-col max-sm:justify-center max-lg:grid-cols-2">
+    <div className="flex">
+      <div className="w-2/3">
+        <p className=" flex justify-center text-xl font-semibold">Jobs</p>
+        <div className="grid grid-cols-3 xl:grid-cols-2 2xl:grid-cols-3 max-lg:flex max-sm:items-center max-lg:flex-col max-sm:justify-center max-xl:grid-cols-2">
           {jobsData &&
             jobsData.jobs &&
             jobsData.jobs.map((job) => {
@@ -82,6 +80,7 @@ const JobsPage = () => {
             })}
         </div>
       </div>
+      <NewJobForm />
     </div>
   );
 };
